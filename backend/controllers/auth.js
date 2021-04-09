406e80196dd57d8e4f52b9969920bd523f71b6b5
Abhinav Bhardwaj <abhinav.bhardwaj_cs18@gla.ac.in>
2021-04-09 11:26:02 +0530

    SignIn Controller

--- backend/controllers/auth.js
+++ backend/controllers/auth.js
@@ -36,3 +36,38 @@ exports.signup = (req, res) => {
 };
 
 
+exports.signin = (req, res) => {
+    const error = validationResult(req);
+    const { email, password } = req.body;
+
+    if(!error.isEmpty()){
+        return res.status(422).json({
+            error: error.array()[0].msg
+        });
+    };
+
+    User.findOne({email}, (err, user) =>{
+        if(err || !user){
+            return res.status(400).json({
+                error: "User's email does not exists!!!"
+            })
+        }
+
+        if(!user.authenticate(password)){
+            return res.status(401).json({
+                error: "Email and password do not match!!!"
+            })
+        }
+
+        //Create token
+        const token = jwt.sign({_id: user._id}, process.env.SECRET_Key);
+        //put token in cookie
+        res.cookie("token", token, {expire: new Date() + 9999});
+
+        //send response on frontend
+        const {_id, firstname, lastname, email, contactNumber, role} = user;
+                
+        return res.json({ token, user: {_id, firstname, lastname, email, contactNumber, role}})
+    })
+
+}
