eebb9749bc4f42d549eb3e22a6a14ceca1f4eaee
Abhinav Bhardwaj <abhinav.bhardwaj_cs18@gla.ac.in>
2021-04-10 13:23:52 +0530

    Adding some validation on signup routes.

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -10,7 +10,9 @@ const { check, validationResult } = require('express-validator');
 router.post(
     "/signup", 
     [
-        
+        check("firstname", "First Name should be at least of 3 char.").isLength({ min: 3 }).custom(value => !/\s/.test(value)),
+        check("email", "Valid email is required.").isEmail().custom(value => !/\s/.test(value)),
+        check("contactNumber", "Valid Mobile Number is required.").isLength({ min: 10 }).isMobilePhone('en-IN'),
         check("password", "password should be at least 5 char.").custom(value => !/\s/.test(value)).isLength({ min: 5 })
     ], 
     signup
