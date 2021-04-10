e32eac73bac194745c5f242416cac13d6e421f34
Abhinav Bhardwaj <abhinav.bhardwaj_cs18@gla.ac.in>
2021-04-11 00:31:59 +0530

    Controller to find product in db using product id.

---
+++ backend/controllers/product.js
@@ -0,0 +1,21 @@
+const Product = require("../models/product");
+const formidable = require("formidable");
+const _ = require("lodash");
+const fs = require("fs");
+const { size, sortBy } = require("lodash");
+
+
+
+exports.getProductById = (req, res, next, id) => {
+    Product.findById(id)
+    .populate("category")
+    .exec((err, product) => {
+        if (err) {
+            return res.status(400).json({
+                error: "Product not found"
+            })
+        }
+        req.product = product;
+        next();
+    });
+};
